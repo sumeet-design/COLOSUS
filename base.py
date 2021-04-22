484656f4eecef63f9005dd6f3e764fd01a0d218d
Sumeet-design <ssumeet2718@gmail.com>
2021-04-22 17:28:17 +0530

    adding some functionality and user conformation

--- base.py
+++ base.py
@@ -1,170 +1,99 @@
 import pyttsx3
-import speech_recognition as sr
-import webbrowser
+import speech_recognition
 import datetime
 import wikipedia
+import webbrowser
+import os
+import smtplib
+
+engine = pyttsx3.init('sapi5')
+voices = engine.getProperty('voices')
+engine.setProperty('voices',voices[0].id)
+engine.runAndWait()
+
+head="avinash"
+# speak function convert  text to audio
+def speak(text):
+    engine.say(text)
+    engine.runAndWait()
+# speak("Intilializing  assistant.....")
+speak("Hello There ,please conform your identity")
 
 
-# this method is for taking the commands
-# and recognizing the command from the
-# speech_Recognition module we will use
-# the recongizer method for recognizing
-def takeCommand():
+def work():
+    r=speech_recognition.Recognizer()
+    with speech_recognition.Microphone() as source:
+        print("listening......")
+        speak("listening.......")
 
-	r = sr.Recognizer()
+        audio = r.listen(source)
 
-	# from the speech_Recognition module
-	# we will use the Microphone module
-	# for listening the command
-	with sr.Microphone() as source:
-		print('Listening')
-		
-		# seconds of non-speaking audio before
-		# a phrase is considered complete
-		r.pause_threshold = 0.7
-		audio = r.listen(source)
-		
-		# Now we will be using the try and catch
-		# method so that if sound is recognized
-		# it is good else we will have exception
-		# handling
-		try:
-			print("Recognizing")
-			
-			# for Listening the command in indian
-			# english we can also use 'hi-In'
-			# for hindi recognizing
-			Query = r.recognize_google(audio, language='en-in')
-			print("the command is printed=", Query)
-			
-		except Exception as e:
-			print(e)
-			speak("sorry avinash i didn't understand")
-			return "None"
-		
-		return Query
+    try :
+        print("thinkingg.....")
+        speak("thinking.....")
+        query=r.recognize_google(audio)
+        print(f"user said: {query}\n")
 
-def speak(audio):
-	
-	engine = pyttsx3.init()
-	# getter method(gets the current value
-	# of engine property)
-	voices = engine.getProperty('voices')
-	
-	# setter method .[0]=male voice and
-	# [1]=female voice in set Property.
-	engine.setProperty('voice', voices[0].id)
-	
-	# Method for the speaking of the the assistant
-	engine.say(audio)
-	
-	# Blocks while processing all the currently
-	# queued commands
-	engine.runAndWait()
+    except Exception as e:
+        print(" repeat again....sorry for inconvenience")
+        print(e)
+        speak("repeat again....sorry for inconvenience")
+        query="none"
+    return query
 
-def tellDay():
-	
-	# This function is for telling the
-	# day of the week
-	day = datetime.datetime.today().weekday() + 1
-	
-	#this line tells us about the number
-	# that will help us in telling the day
-	Day_dict = {1: 'Monday', 2: 'Tuesday',
-				3: 'Wednesday', 4: 'Thursday',
-				5: 'Friday', 6: 'Saturday',
-				7: 'Sunday'}
-	
-	if day in Day_dict.keys():
-		day_of_the_week = Day_dict[day]
-		print(day_of_the_week)
-		speak("The day is " + day_of_the_week)
+#work()
+
+query=work()
+if 'sumit' in query.lower():
+    head = 'sumeet'
+else:
+    head = 'avinash'
+def wishme():
+    hour = int(datetime.datetime.now().hour)
+    #print(hour)
+    if hour>=0 and hour <12:
+        speak("good morning "+head+".........how may i help you")
+
+    elif hour>=12 and hour<18:
+        speak("good afternoon "+head+".........how may i help you")
+
+    else:
+        speak("good evening "+head+".........how may i help you")
+wishme()
+query = work()
+#adding functionality for doing basic work................
+if 'wikipedia' in query.lower():
+    speak('Searching Wikipedia......')
+    query=query.replace("Wikipedia","")
+    result=wikipedia.summary(query , sentences=2)
+    print(result)
+    speak(result)
+elif 'open youtube' in query.lower():
+    webbrowser.open("youtube.com")
+    # query = work()
+elif 'open google' in query.lower():
+    webbrowser.open("google.com")
+elif 'open amazon' in query.lower():
+    webbrowser.open("amazon.com")
+elif 'today news' in query.lower():
+    webbrowser.open("https://www.indiatoday.in/news.html")
+elif  'search' in query.lower():
+    x=query.replace("search","")
+    webbrowser.open(x)
+elif 'play music' in query.lower():
+    music="D:\\OLD SONG"
+    songs=os.listdir(music)
+    os.startfile(os.path.join(music,songs[4]))
+elif 'photo' in query.lower():
+    photo="D:\\photos"
+    images=os.listdir(photo)
+    os.startfile(os.path.join(photo,images[1]))
+elif 'the time' in query.lower():
+    time=datetime.datetime.now().strftime("%H:%M:%S")
+    speak(f"Rimjhim man time is{time}")
+query = work()
 
 
-def tellTime():
-	
-	# This method will give the time
-	time = str(datetime.datetime.now())
-	
-	# the time will be displayed like
-	# this "2020-06-05 17:50:14.582630"
-	#nd then after slicing we can get time
-	print(time)
-	hour = time[11:13]
-	min = time[14:16]
-	speak(self, "The time is " + hour + "Hours and" + min + "Minutes")	
-
-def Hello():
-	
-	# This function is for when the assistant
-	# is called it will say hello and then
-	# take query
-	speak("hello   avinash     this   is   mizz .   Tell me how may I help you")
 
 
-def Take_query():
 
-	# calling the Hello function for
-	# making it more interactive
-	Hello()
-	
-	# This loop is infinite as it will take
-	# our queries continuously until and unless
-	# we do not say bye to exit or terminate
-	# the program
-	while(True):
-		
-		# taking the query and making it into
-		# lower case so that most of the times
-		# query matches and we get the perfect
-		# output
-		query = takeCommand().lower()
-		if "open geeksforgeeks" in query:
-			speak("Opening GeeksforGeeks ")
-			
-			# in the open method we just to give the link
-			# of the website and it automatically open
-			# it in your default browser
-			webbrowser.open("www.geeksforgeeks.com")
-			continue
-		
-		elif "open google" in query:
-			speak("Opening Google ")
-			webbrowser.open("www.google.com")
-			continue
-			
-		elif "which day it is" in query:
-			tellDay()
-			continue
-		
-		elif "tell me the time" in query:
-			tellTime()
-			continue
-		
-		# this will exit and terminate the program
-		elif "bye" in query:
-			speak("Bye Bye avinash have a nice day.")
-			exit()
-		
-		elif "from wikipedia" in query:
-			
-			# if any one wants to have a information
-			# from wikipedia
-			speak("Checking the wikipedia ")
-			query = query.replace("wikipedia", "")
-			
-			# it will give the summary of 4 lines from
-			# wikipedia we can increase and decrease
-			# it also.
-			result = wikipedia.summary(query, sentences=4)
-			speak("According to wikipedia")
-			speak(result)
-		
-		elif "tell me your name" in query:
-			speak("I am mizz. Your deskstop Assistant")
-
-if __name__ == '__main__':
-	
-	# main method for executing
-	# the functions
-	Take_query()
---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,11 @@
+def wishme():
+#     hour = int(datetime.datetime.now().hour)
+#     #print(hour)
+#     if hour>=0 and hour <12:
+#         speak("good morning "+head+".........how may i help you")
+
+#     elif hour>=12 and hour<18:
+#         speak("good afternoon "+head+".........how may i help you")
+
+#     else:
+#         speak("good evening "+head+".........how may i help you")
